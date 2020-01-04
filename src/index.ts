d8369235cdfc76baa38fcea980d2680899b54821
jyp <jyp@notiplus.co.kr>
2020-01-04 22:49:53 +0900

    class and object

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
-const group = "ABBO",
-nowYear = 2020,
-groupMessage = "Happy new Year!!";
+class Abbo {
+    private group : string;
+    private nowYear: number;
+    public groupMessage: string;
+    constructor(groupMessage: string) {
+        this.group = "ABBO";
+        this.nowYear = new Date().getFullYear();
+        this.groupMessage = groupMessage;
+    }
 
-const helloWorld = (group: string, nowYear: number, groupMessage: string): string => {
-    let message = `Hello ${group}, This year is ${nowYear}. ${groupMessage}`
+    public getNowYear() {
+        return this.nowYear;
+    }
+
+    public getGroup() {
+        return this.group;
+    }
+}
+
+const niyoung2 = new Abbo("Happy new Year!!");
+
+const helloWorld = (team: Abbo): string => {
+    let message = `Hello ${team.getGroup}, This year is ${team.getNowYear}. 
+    ${team.groupMessage}`
     return message
 };
 
 
-console.log(helloWorld(group, nowYear, groupMessage))
+console.log(helloWorld(niyoung2))
 export {};
